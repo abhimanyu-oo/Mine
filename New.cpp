9bc1fadda1cd1c6497ef668c7391cc909a3cebc1
abhimanyu-oo <abhi.13.09.2007@gmail.com>
2025-08-10 16:09:31 +0530

    File

--- New.cpp
+++ New.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 void New() {
     println ("Connected via Void function!!");
+    println();
+
     char Grade;
     Grade = 'A';
     cout << Grade << endl;
@@ -10,4 +12,25 @@ void New() {
     cout << Grade << endl;
     Grade = 'C';
     cout << Grade << endl;
+    Grade = 'D';
+    cout << Grade << endl;
+    Grade = 'E';
+    cout << Grade << endl;
+    Grade = 'F';
+    cout << Grade << endl;
+    cout<<endl;
+
+    double pi = 3.141592653589793;
+    cout << "The Value of pi is "<< pi << endl;
+    cout<<endl;
+
+    bool is_correct = true;
+    cout << is_correct << endl;
+    is_correct = false;
+    cout << is_correct << endl;
+    cout<<endl;
+
+    int age = 18;
+    cout << "My age is "<< age << endl;
+    cout<<endl;
 }
