2b1083dff96440e6343509affd9dcc2774fde368
abhimanyu-oo <abhi.13.09.2007@gmail.com>
2025-08-10 16:05:31 +0530

    File

--- New.cpp
+++ New.cpp
@@ -3,4 +3,11 @@
 using namespace std;
 void New() {
     println ("Connected via Void function!!");
+    char Grade;
+    Grade = 'A';
+    cout << Grade << endl;
+    Grade = 'B';
+    cout << Grade << endl;
+    Grade = 'C';
+    cout << Grade << endl;
 }
