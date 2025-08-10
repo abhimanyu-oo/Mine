7c5c181b68ac07d90a0412ed6fe3e4233c52cb37
abhimanyu-oo <abhi.13.09.2007@gmail.com>
2025-08-10 16:15:51 +0530

    Learning Variables and String Functions

--- New.cpp
+++ New.cpp
@@ -18,23 +18,20 @@ void New() {
     cout << Grade << "\n";
     Grade = 'F';
     cout << Grade << "\n";
-    cout<<endl;
 
     double pi = 3.141592653589793;
     cout << "The Value of pi is "<< pi << endl;
-    cout<<endl;
 
     bool is_correct = true;
     cout << is_correct << endl;
     is_correct = false;
     cout << is_correct << endl;
-    cout<<endl;
 
     int age = 18;
     cout << "My age is "<< age << "\n";
-    cout<<endl;
 
     string name = "Abhimanyu";
     cout << "My name is " << name << endl;
-    cout<<endl;
+
+    cout << "First Letter of name is " << name[0] << endl;
 }
