5f96b87458d87017b6f1e2821aca35375e9e1d6c
abhimanyu-oo <abhi.13.09.2007@gmail.com>
2025-08-10 17:14:28 +0530

    Madlibs

--- New.cpp
+++ New.cpp
@@ -1,8 +1,10 @@
 # include <iostream>
 # include <print>
+# include <cmath>
 using namespace std;
 void New() {
     println ("Connected via Void function!!");
+    // Variables
 
     // char Grade;
     // Grade = 'A';
@@ -32,6 +34,7 @@ void New() {
     // string name = "Abhimanyu";
     // cout << "My name is " << name << endl;
 
+    // String Commands
     // cout << "First Letter of name is " << name[0] << endl;
     // name[0]='x'; // Changes the the letter in given position to provided letter, x in this case.
     // cout << "My name with First letter as x is " << name << endl;
@@ -39,8 +42,47 @@ void New() {
     // cout << name.find("man" , 0) << "\n"; // tells the position after which the thing to be found occurs.
     // cout << name.substr(4,6) << endl; // Prints a part of the string between given defined positions.
 
+    // Int Commands
+    // int num1 = 3;
+    // double num2 = 4;
+    // double num3 = 3.41;
+    // cout << pow(num1,num2) << endl; // raises the power 3 to power of 4 in this case.
+    // cout << num1 + num2 << endl; // Addition
+    // cout << num1 - num2 << endl; // Subtraction
+    // cout << num1 * num2 << endl; // Multiplication
+    // cout << num1 / num2 << endl; // division(but if the input is int , answer is also int , so for better answers make either of them double or float.)
+    // cout << num1 % 4 << endl; // Remainder
+    // cout << sqrt(num2) << endl; // Square root
+    // cout << round(num3)<< endl; // Round Off
+    // cout << ceil(num3) << endl; // Round up
+    // cout << floor(num3) << endl; // Round Down or GIF
+    // cout << fmax(num1,num2) << endl; // Max of numbers
+    // cout << fmin(num1,num2) << endl; // Min of Numbers
 
+    // Taking Input from User
+    // string age; // Similar for Char
+    // cout << "Enter your age: ";
+    // getline(cin, age);
+    // cout << "You told me that you are " << age << " years old" <<endl;
 
+    // string Name;
+    // cout << "Enter your name: ";
+    // getline(cin, Name);
+    // cout << "Hello Mr. " << Name << endl;
 
+    // A MadLibs Game
+    // string color,noun,celeb;
+    // cout<<"Enter your favourite color : ";
+    // getline(cin,color);
+    // cout<<"Enter a noun : ";
+    // getline(cin,noun);
+    // cout<<"Enter your favourite celeb : ";
+    // getline(cin,celeb);
+    //
+    // cout << "Rosses are " << color << endl;
+    // cout << noun <<" are Blue" << endl;
+    // cout << "I love "<< celeb << endl;
+
+    // Arrays
 
 }
